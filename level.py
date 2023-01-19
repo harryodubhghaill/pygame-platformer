9d6b72bd2b71887cffb696e0110ebb38720a1808
Harry Doyle <harryodubhghaill@hotmail.com>
2023-01-19 19:44:14 +0000

    cut tilesets and draw onto surface

--- level.py
+++ level.py
@@ -1,13 +1,14 @@
 import pygame
-from tiles import Tile
+from tiles import Tile, StaticTile
 from settings import tile_size, screen_limit_right, screen_limit_left
 from player import Player
 from particles import ParticleEffect
-from support import import_csv_layout
+from support import import_csv_layout, import_cut_graphics
 
 class Level:
     def __init__(self, level_data, surface):
 
+        # terrain setup
         terrain_layout = import_csv_layout(level_data['terrain'])
         self.terrain_sprites = self.create_tile_group(terrain_layout, 'terrain')
 
@@ -31,7 +32,9 @@ class Level:
                     y = row_index * tile_size
 
                     if type == 'terrain':
-                        sprite = Tile(tile_size, x, y)
+                        terrain_tile_list = import_cut_graphics('./graphics/terrain/terrain_tiles.png')
+                        tile_surface = terrain_tile_list[int(val)]
+                        sprite = StaticTile(tile_size, x, y, tile_surface)
                         sprite_group.add(sprite)
 
         return sprite_group
@@ -139,8 +142,8 @@ class Level:
 
         # level tiles
         self.terrain_sprites.draw(self.display_surface)
-        self.tiles.update(self.world_shift)
-        self.tiles.draw(self.display_surface)
+        # self.tiles.update(self.world_shift)
+        # self.tiles.draw(self.display_surface)
         # self.scroll_x()
 
         # player
