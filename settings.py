305acce0e44789700f1909177ec571817b7a0148
Harry Doyle <harryodubhghaill@hotmail.com>
2023-01-23 19:44:35 +0000

    clean formatting

--- settings.py
+++ settings.py
@@ -1,16 +1,3 @@
-# level_map = [
-# '                            ',
-# '                            ',
-# '                            ',
-# ' XX    XXX            XX    ',
-# ' XX P                       ',
-# ' XXXX         XX         XX ',
-# ' XXXX       XX              ',
-# ' XX    X  XXXX    XX  XX    ',
-# '       X  XXXX    XX  XXX   ',
-# '    XXXX  XXXXXX  XX  XXXX  ',
-# 'XXXXXXXX  XXXXXX  XX  XXXX  ']
-
 tile_size = 64
 vertical_tile_number = 11 #from tiled data
 
