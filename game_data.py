7a6dff9b1616a03d5ca11f7963f859b4ccfc662d
Harry Doyle <harryodubhghaill@hotmail.com>
2023-01-24 00:25:36 +0000

    add graphics for overworld

--- game_data.py
+++ game_data.py
@@ -1,6 +1,7 @@
 level_0 = {
 	'node_pos': (110, 400),
 	'content': 'This is level 0',
+	'node graphics': './graphics/overworld/0',
 	'unlock': 1,
     'terrain': './levels/0/level_0_terrain.csv',
 	'coins':'./levels/0/level_0_coins.csv',
@@ -16,6 +17,7 @@ level_0 = {
 level_1 = {
 	'node_pos': (300, 220),
 	'content': 'This is level 1',
+	'node graphics': './graphics/overworld/1',
 	'unlock': 2,
     'terrain': './levels/0/level_0_terrain.csv',
 	'coins':'./levels/0/level_0_coins.csv',
@@ -31,6 +33,7 @@ level_1 = {
 level_2 = {
 	'node_pos': (480, 610),
 	'content': 'This is level 2',
+	'node graphics': './graphics/overworld/2',
 	'unlock': 3,
     'terrain': './levels/0/level_0_terrain.csv',
 	'coins':'./levels/0/level_0_coins.csv',
@@ -46,6 +49,7 @@ level_2 = {
 level_3 = {
 	'node_pos': (610, 350),
 	'content': 'This is level 3',
+	'node graphics': './graphics/overworld/3',
 	'unlock': 4,
     'terrain': './levels/0/level_0_terrain.csv',
 	'coins':'./levels/0/level_0_coins.csv',
@@ -61,6 +65,7 @@ level_3 = {
 level_4 = {
 	'node_pos': (880, 210),
 	'content': 'This is level 4',
+	'node graphics': './graphics/overworld/4',
 	'unlock': 5,
     'terrain': './levels/0/level_0_terrain.csv',
 	'coins':'./levels/0/level_0_coins.csv',
@@ -76,6 +81,7 @@ level_4 = {
 level_5 = {
 	'node_pos': (1050, 400),
 	'content': 'This is level 5',
+	'node graphics': './graphics/overworld/5',
 	'unlock': 5,
     'terrain': './levels/0/level_0_terrain.csv',
 	'coins':'./levels/0/level_0_coins.csv',
