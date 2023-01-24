0760fe58195d4aa293761aadfe295e41fc70d8f9
Harry Doyle <harryodubhghaill@hotmail.com>
2023-01-24 01:41:33 +0000

    fix 0 level overworld draw path

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ from game_data import levels
 
 class Game:
     def __init__(self):
-        self.max_level = 2
-        self.overworld = Overworld(1, self.max_level, screen, self.create_level)
+        self.max_level = 0
+        self.overworld = Overworld(0, self.max_level, screen, self.create_level)
         self.status = 'overworld'
 
     def create_level(self, current_level):
